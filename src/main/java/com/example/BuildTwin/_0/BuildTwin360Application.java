31bb184ab42000819de8f883ffb3f1a9354ff29a
triovexa <triovexa@gmail.com>
2026-08-24 11:48:25 +0530

    first commit

---
+++ src/main/java/com/example/BuildTwin/_0/BuildTwin360Application.java
@@ -0,0 +1,13 @@
+package com.example.BuildTwin._0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTwin360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildTwin360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuildTwin/_0/BuildTwin360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuildTwin._0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildTwin360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
